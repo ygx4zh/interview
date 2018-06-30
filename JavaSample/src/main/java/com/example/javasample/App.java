271eb6d00efb2b30e09cdcb78b93e96a83ee2fe2
YGX <sinsyet@163.com>
2018-06-30 10:33:03 +0800

    add

---
+++ JavaSample/src/main/java/com/example/javasample/App.java
@@ -0,0 +1,8 @@
+package com.example.javasample;
+
+public class App {
+    public static void main(String[] args){
+        double sqrt = Math.sqrt(100);
+        System.out.println(sqrt);
+    }
+}
